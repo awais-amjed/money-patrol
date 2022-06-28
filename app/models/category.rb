91a8ab9d79515f3520e0ffe996cb4a068cbbeb1f
Awais Amjed <awaisamjed171@gmail.com>
2022-06-28 16:58:10 +0500

    Add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :expenses, dependent: :destroy
+
+  validates :name, presence: true
+  validates :icon, presence: true
+  validates :author_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_112958) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_114937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_112958) do
     t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_expenses_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -30,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_112958) do
   end
 
   add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "expenses", "categories"
 end
