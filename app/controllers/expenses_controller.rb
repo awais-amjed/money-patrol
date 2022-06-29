9b88ccf1dfe241ec76b402d83ea626b96157f182
Awais Amjed <awaisamjed171@gmail.com>
2022-06-29 16:09:56 +0500

    Generate scaffold for expenses

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,55 @@
+class ExpensesController < ApplicationController
+  before_action :set_expense, only: %i[ destroy ]
+  before_action :set_category
+  before_action :set_user, only: %i[ new ]
+
+  # GET /expenses/new
+  def new
+    @expense = Expense.new
+  end
+
+  # POST /expenses or /expenses.json
+  def create
+    @expense = Expense.new(expense_params)
+
+    respond_to do |format|
+      if @expense.save
+        format.html { redirect_to category_url(@category), notice: "Expense was successfully created." }
+        format.json { render :show, status: :created, location: @expense }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @expense.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /expenses/1 or /expenses/1.json
+  def destroy
+    @expense.destroy
+
+    respond_to do |format|
+      format.html { redirect_to category_url(@category), notice: "Expense was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  # Only allow a list of trusted parameters through.
+  def expense_params
+    params.require(:expense).permit(:name, :amount, :category_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:index, :show, :new, :create, :destroy]
+  resources :categories, only: [:index, :show, :new, :create, :destroy] do
+    resources :expenses, only: [:new, :create, :destroy]
+  end
 
   # Defines the root path route ("/")
   root 'categories#index'
