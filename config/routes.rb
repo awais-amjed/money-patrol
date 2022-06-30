a101e809679e344835990b2b89813a10c37056d5
Awais Amjed <awaisamjed171@gmail.com>
2022-06-30 21:03:34 +0500

    Add splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show, :new, :create, :destroy] do
     resources :expenses, only: [:new, :create, :destroy]
   end
+  resources :splash, only: [:index]
 
   # Defines the root path route ("/")
-  root 'categories#index'
+  root 'splash#index'
 end
