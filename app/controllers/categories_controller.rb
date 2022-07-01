a8e332081ec06402c4f92ca0405ad5f460c95b44
Awais Amjed <awaisamjed171@gmail.com>
2022-07-01 15:25:34 +0500

    Order Expenses by most recent

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,7 +11,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1 or /categories/1.json
   def show
-    @expenses = @category.expenses
+    @expenses = @category.expenses.order(created_at: :desc)
   end
 
   # GET /categories/new
